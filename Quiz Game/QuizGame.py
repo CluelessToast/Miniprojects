3d973478ac2147996bcdfb56ea72693f36fad0ee
CluelessToast <88457405+CluelessToast@users.noreply.github.com>
2022-02-20 12:45:27 -0500

    completed game

--- Quiz Game/QuizGame.py
+++ Quiz Game/QuizGame.py
@@ -1,3 +1,44 @@
+from tkinter.messagebox import YES
+
+
 print("Welcome to the quiz.")
 
 play= input("Would you like to play a game? :) ");
+score=0
+
+
+if play.lower() !="yes":
+    quit();
+
+print("Okay, let's go!")
+
+answer=input("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score+=1
+else:
+    print('Incorrect :(')
+#-------------
+answer=input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score+=1
+else:
+    print('Incorrect :(')
+
+#-------------
+answer=input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score+=1
+else:
+    print('Incorrect :(')
+#-------------
+answer=input("What does PSU stands for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score+=1
+else:
+    print('Incorrect :(')
+
+print('your score is '+ str(score/4*100)+ "%!");
