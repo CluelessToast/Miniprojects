5e71d1c8ab531e69893996759dcb0b7b43189878
CluelessToast <88457405+CluelessToast@users.noreply.github.com>
2022-02-21 06:50:44 -0500

    init

---
+++ _passmanager/password_manager.py
@@ -0,0 +1,24 @@
+pwd= input("What is the master password?")
+
+def view():
+    pass
+def add():
+    pass
+
+
+
+
+while True:
+    mode= input("Would you like to add a new password or view existing ones?. Press q to quit.")
+
+    if mode == "q":
+        break
+
+
+    if mode.lower() == "view":
+        pass
+    elif mode.lower()=="add":
+        pass
+    else:
+        print("Invalid mode.")
+        continue
